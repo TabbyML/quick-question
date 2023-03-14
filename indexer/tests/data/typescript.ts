27ae461342a8e62dc4c9644f0f52820da8fedd05
icycodes <exile.icy@gmail.com>
2023-03-14 13:14:04 +0800

    Add typescript parser, try indexing this repo itself. (#6)
    * Add typescript parser, try indexing this repo itself.
    
    * Change typescript parsing maxLevel to 1. Add testing.
    
    * Update testing description. Resolve lint problem.
    
    ---------
    
    Co-authored-by: Zhiming Ma <zhiming@pixelshift.ai>

---
+++ indexer/tests/data/typescript.ts
@@ -0,0 +1,32 @@
+function logWithDividing(object: any) {
+
+  function log(object: any) {
+    // Nested funciton should not be indexed.
+    console.log(object);
+  }
+
+  const div: string = "------";
+  log(div);
+  log(object);
+}
+
+function shortFunction() {
+  // Too short to be indexed.
+}
+
+class Greeter {
+  greeting: string;
+
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  greet(): string {
+    return "Hello, " + this.greeting;
+  }
+}
+
+let greeter = new Greeter("world");
+logWithDividing(greeter.greet());
+
+export {};
--- indexer/tests/parser.spec.ts
+++ indexer/tests/parser.spec.ts
@@ -2,11 +2,20 @@ import path from "path";
 import assert from "assert";
 import { parseFile } from "../src/parser";
 
-describe("parseFile", function () {
+describe("parseFile: python", function () {
   it("should success", async function () {
     const chunks = await parseFile("./tests/data/python.py");
     assert.equal(chunks.length, 2);
     assert.match(chunks[0].code, /def fib.*/);
     assert.match(chunks[1].code, /class Foo.*/);
+  });
+});
+
+describe("parseFile: typescript", function () {
+  it("should success", async function () {
+    const chunks = await parseFile("./tests/data/typescript.ts");
+    assert.equal(chunks.length, 2);
+    assert.match(chunks[0].code, /function logWithDividing.*/);
+    assert.match(chunks[1].code, /class Greeter.*/);
   });
 });
